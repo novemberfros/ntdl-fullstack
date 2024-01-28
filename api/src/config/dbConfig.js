6bcafe932def26c054ea4c75d4b52fdc088cc579
novemberfros <deependrapathak2@gmail.com>
2024-01-28 11:54:58 +1100

    new Build ntdl

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-const mongo_db_url = "mongodb://localhost:27017/ntdl-db"
+const mongo_db_url =
+  process.env.MONGO_DB_URL || "mongodb://localhost:27017/ntdl-db";
 
 export const connectMongo = () => {
   try {
-    const connect = mongoose.connect(mongo_db_url)
-    if(connect) {
+    const connect = mongoose.connect(mongo_db_url);
+    if (connect) {
       console.log("Database conected at" + mongo_db_url);
     }
   } catch (error) {
     console.log("Error:", error);
   }
-}
+};
